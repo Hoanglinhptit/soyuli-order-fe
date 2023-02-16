ab62738dc508bc7ac37dbb13e421c3f583c35049
Your Name <you@example.com>
2023-02-16 21:47:58 +0700

    basic browse

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Helmet } from 'react-helmet';
 import { useState } from 'react';
 import Header from './layouts/header/Header';
 import Login from './layouts/account/login/Login'
-
+import Browse from './layouts/browse/Browse'
 
 export  default function App() {
   const [value, setValue] = useState<string>('');
@@ -21,7 +21,8 @@ export  default function App() {
     <div>
       <Helmet><title>Soyuli Order</title></Helmet>
       <Header onSearch={onSearch} onChange={onChange} value={value} onSeclect={onSelect}/>
-      <Login></Login>
+      {/* <Login></Login> */}
+      <Browse></Browse>
     </div>
   );
 }
