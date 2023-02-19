14043a8aedade7f97d8b056cbcbc3c96e8ccb25d
Hoang Linh <linh.lh@tinasoft.vn>
2023-02-19 21:15:43 +0700

    route-config

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import { HashRouter, Route, Routes } from 'react-router-dom';
+/* eslint-disable react/jsx-key */
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+import { HashRouter, Route, Routes, useLocation } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Header from './layouts/header/Header';
 import Footer from './layouts/footer/Footer';
 import Browse from './layouts/browse/Browse';
 import SOYULI_ROUTES from './routes/route';
 import ProtectedRoute from './routes/ProtectedRoute';
+import Payment from './pages/payment'
 
 export  default function App() {
+  const [cond, setCond] = useState<boolean>(window.location.pathname === "/login" || window.location.pathname === "/register");
+    const location = useLocation();
+    useEffect(() => {
+        console.log("location: ", location)
+        const cond = location.pathname === "/login" || location.pathname === "/register"
+        cond ? setCond(true) : setCond(false)
+    }, [location])
   const [value, setValue] = useState<string>('');
   const onSelect = (value: string) => {
     setValue(value);
@@ -22,9 +32,16 @@ export  default function App() {
   return (
     <div>
       <Helmet><title>Soyuli Order</title></Helmet>
-      <Header onSearch={onSearch} onChange={onChange} value={value} onSeclect={onSelect}/>
+     {!cond && <Header onSearch={onSearch} onChange={onChange} value={value} onSeclect={onSelect}/>} 
       {/* <Login></Login> */}
+      <Routes>
+      {
+        SOYULI_ROUTES.map((e)=><Route path={e.path} element={e.element}/>)
+      } 
+      <ProtectedRoute path='/payment' component={Payment}/> 
+      </Routes>
       <Browse/>
+      
       <Footer/>
     </div>
   );
